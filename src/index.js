94c075406a4ad4503c28e69745c58d22ba6395ba
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-02-25 18:20:01 +0400

    Add function for fetching API data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./style.css";
+
+async function fetchWeatherData(location) {
+	const apiKey = "PAASE59UM8JGVJWS2Y9JSEUVT";
+	const unitGroup = "metric";
+	const contentType = "json";
+	const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?unitGroup=${unitGroup}&contentType=${contentType}&key=${apiKey}`;
+
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+		const data = await response.json();
+
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.error("Failed to fetch weather data:", error);
+	}
+}
+
+fetchWeatherData("London");
